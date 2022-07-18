a96c98a598d25f36cfdcedeb44dc9e2793031756
Art Semyonov <art@mycelial.com>
2022-07-18 20:01:57 +0300

    Add nodejs worker

---
+++ worker/index.mjs
@@ -0,0 +1,50 @@
+import * as Mycelial from '@mycelial/core';
+import path from 'path';
+import fs from 'fs';
+import url from 'url';
+import * as Websocket from './websocket.mjs';
+
+
+(async function() {
+  const mycelial = await Mycelial.create("namespace", Math.floor(Math.random() * 100000000), {
+    resolver: () => {
+      const module = path.join(
+        path.dirname(url.fileURLToPath(import.meta.url)),
+        'node_modules/@mycelial/wasm/dist/index_bg.wasm'
+      );
+
+      return fs.readFileSync(module)
+    }
+  });
+
+  const ws = Websocket.create(mycelial, {
+    endpoint: 'wss://v0alpha-relay.fly.dev/v0alpha'
+  });
+
+  mycelial.events.addEventListener('update', (evt) => {
+    console.log('update', evt);
+    console.log(mycelial)
+
+    console.log(mycelial.log.to_vec())
+  });
+
+  mycelial.events.addEventListener('apply', (evt) => {
+    console.log('apply', evt);
+    console.log(mycelial)
+
+    console.log(mycelial.log.to_vec())
+  });
+
+  console.log(mycelial.log.to_vec())
+
+  mycelial.commit([
+    {
+      $id: "id",
+      attribute: "value"
+    }
+  ]);
+
+  setInterval(() => {
+    console.log('The timer keeps the process running');
+  }, 1000 * 60 * 60);
+})()
