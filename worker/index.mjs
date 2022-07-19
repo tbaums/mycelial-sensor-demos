ce75a3dc1c951832b0add361919d05e047cbafd3
Michael Tanenbaum <m.tanenbaum@icloud.com>
2022-07-19 10:39:35 -0400

    experiment

--- worker/index.mjs
+++ worker/index.mjs
@@ -1,50 +1,60 @@
-import * as Mycelial from '@mycelial/core';
-import path from 'path';
-import fs from 'fs';
-import url from 'url';
-import * as Websocket from './websocket.mjs';
+import * as Mycelial from "@mycelial/core";
+import path from "path";
+import fs from "fs";
+import url from "url";
+import * as Websocket from "./websocket.mjs";
 
+(async function () {
+  const mycelial = await Mycelial.create(
+    "tbaums-rpi-4",
+    Math.floor(Math.random() * 100000000),
+    {
+      resolver: () => {
+        const module = path.join(
+          path.dirname(url.fileURLToPath(import.meta.url)),
+          "node_modules/@mycelial/wasm/dist/index_bg.wasm"
+        );
 
-(async function() {
-  const mycelial = await Mycelial.create("namespace", Math.floor(Math.random() * 100000000), {
-    resolver: () => {
-      const module = path.join(
-        path.dirname(url.fileURLToPath(import.meta.url)),
-        'node_modules/@mycelial/wasm/dist/index_bg.wasm'
-      );
-
-      return fs.readFileSync(module)
+        return fs.readFileSync(module);
+      },
     }
-  });
+  );
 
   const ws = Websocket.create(mycelial, {
-    endpoint: 'wss://v0alpha-relay.fly.dev/v0alpha'
+    endpoint: "wss://v0alpha-relay.fly.dev/v0alpha",
   });
 
-  mycelial.events.addEventListener('update', (evt) => {
-    console.log('update', evt);
-    console.log(mycelial)
+  mycelial.events.addEventListener("update", (evt) => {
+    console.log("update", evt);
+    // console.log(mycelial)
 
-    console.log(mycelial.log.to_vec())
+    console.log(mycelial.log.to_vec());
   });
 
-  mycelial.events.addEventListener('apply', (evt) => {
-    console.log('apply', evt);
-    console.log(mycelial)
+  mycelial.events.addEventListener("apply", (evt) => {
+    console.log("apply", evt);
+    // console.log(mycelial)
 
-    console.log(mycelial.log.to_vec())
+    console.log(mycelial.log.to_vec());
   });
 
-  console.log(mycelial.log.to_vec())
+  console.log(mycelial.log.to_vec());
 
   mycelial.commit([
     {
       $id: "id",
-      attribute: "value"
-    }
+      color: "red",
+    },
+  ]);
+
+  mycelial.commit([
+    {
+      $id: "id",
+      color: "blue",
+    },
   ]);
 
   setInterval(() => {
-    console.log('The timer keeps the process running');
+    console.log("The timer keeps the process running");
   }, 1000 * 60 * 60);
-})()
+})();
